4862ff309eac8b1964e921cddfbe5791394218eb
Igor Bojczuk <igorbojczuk@Igors-MacBook-Pro.local>
2022-01-26 23:15:03 +0100

    includes script hidden, CORS allowed

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-include 'crypto.php';
+include 'includes/crypto.php';
 header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 $encodedApiKey = htmlspecialchars($_GET["apikey"]);
 $decodedApiKey = Crypto::decrypt_openssl($encodedApiKey);
 $response = array(
